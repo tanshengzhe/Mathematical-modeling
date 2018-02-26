5a7d9758a4ded906969651b29279c2b7c0da882b
tanshengzhe <tanshengzhe@users.noreply.github.com>
2018-02-26 18:35:56 +0800

    kruskal函数

---
+++ matlab/网络优化问题/kruskal_f.m
@@ -0,0 +1,23 @@
+function [ result ] = kruskal_f( a )
+[i,j,b]=find(a);
+data=[i';j';b'];
+index=data(1:2,:);
+result=[];
+loop=length(a)-1;
+while length(result)<loop
+    temp=min(data(3,:));
+    flag=find(data(3,:)==temp);
+    flag=flag(1);
+    v1=index(1,flag);v2=index(2,flag);
+    if v1~=V2
+        result=[result,temp];
+    end
+    index(find(index==v2))=v1;
+    index(:,flag)=[];
+    data(:,flag)=[];
+end
+%   Detailed explanation goes here
+
+
+end
+
